ac648e2a50f64292ffe825df5e5e381f3ebe63a2
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2022-03-26 22:38:45 +0600

    sidebar collapse issue fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import { Routes, Route, Navigate } from "react-router-dom";
 import Authors from '../views/App/Authors';
 import FavoriteAuthors from '../views/App/FavoriteAuthors';
 import Sidebar from '../views/Components/Sidebar';
 
 export default function Router() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <Row className='horizontalRootMrgn'>
-      <Col md={3}>
-          <Sidebar/>
+      <Col md={collapsed === true ? 2 : 3}>
+          <Sidebar collapsed={collapsed} setCollapsed={setCollapsed}/>
       </Col>
-      <Col md={9}>
+      <Col md={collapsed === true ? 10 : 9}>
+          {/* sidebar manage for responsive design */}
+         {
+           window.screen.width < 746 && ( 
+            <div onClick={() => setCollapsed(!collapsed)} style={{cursor: "pointer", padding: "20px 0px 0px 20px"}}>
+                <GiHamburgerMenu/>  
+            </div>
+            )
+         }
          <Routes>
              <Route path='/' element={<Navigate to="/authors"/>}/>
              <Route path='/authors' element={<Authors/>}/>
--- src/views/Components/Sidebar/index.js
+++ src/views/Components/Sidebar/index.js
@@ -3,12 +3,15 @@ import styles from "../../../assets/css/sidebar.module.css";
 import { FaUsers } from "react-icons/fa";
 import { MdFavorite } from "react-icons/md";
 import { Link } from 'react-router-dom';
+import { GiHamburgerMenu } from "react-icons/gi";
 
-
-export default function Sidebar() {
+export default function Sidebar({collapsed, setCollapsed}) {
 
   return (
-    <div className={styles.sidebarWrapper}>
+    <div className={collapsed === true ? styles.collapsedSidebarWrapper : styles.sidebarWrapper}>
+      <div onClick={() => setCollapsed(!collapsed)} style={{cursor: "pointer"}}>
+          <GiHamburgerMenu/>  
+      </div>
         <Link to="/authors" className={styles.navLink}>
             <h3 className='mb-5 text-center'>Author Management</h3>
         </Link>
